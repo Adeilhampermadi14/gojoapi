20e8687f92b0f9bd38452185802d57fcbf698926
soerahmat69 <surahmate38@gmail.com>
2023-07-02 10:22:06 +0800

    pdate

--- testoy.go
+++ testoy.go
@@ -6,77 +6,74 @@ import (
 	"os"
 	"time"
 
+	"github.com/aiteung/atdb"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var MongoString string = os.Getenv("MONGOSTRING")
 
-func MongoConnect(dbname string) (db *mongo.Database) {
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(MongoString))
-	if err != nil {
-		fmt.Printf("MongoConnect: %v\n", err)
-	}
-	return client.Database(dbname)
+var MongoInfo = atdb.DBInfo{
+	DBString: MongoString,
+	DBName:   "presensiMahasiswa",
 }
 
-func InsertOneDoc(db string, collection string, doc interface{}) (insertedID interface{}) {
-	insertResult, err := MongoConnect(db).Collection(collection).InsertOne(context.TODO(), doc)
+var MongoConn = atdb.MongoConnect(MongoInfo)
+
+func InsertOneDoc(db *mongo.Database, collection string, doc interface{}) (insertedID interface{}) {
+	insertResult, err := db.Collection(collection).InsertOne(context.TODO(), doc)
 	if err != nil {
 		fmt.Printf("InsertOneDoc: %v\n", err)
 	}
 	return insertResult.InsertedID
 }
 
-func InsertPresensi(checkin string, id_mhs string) (InsertedID interface{}) {
+func InsertPresensi(db *mongo.Database, checkin string, id_mhs string) (InsertedID interface{}) {
 	var presensi Presensi
 	presensi.Datetime = primitive.NewDateTimeFromTime(time.Now().UTC())
 	presensi.Checkin = checkin
 	presensi.ID_mhs = id_mhs
-	return InsertOneDoc("dbmhs", "presensi", presensi)
+	return InsertOneDoc(db, "presensi", presensi)
 }
 
-func InsertMahasiswa(nama string, phone_number string, prodi string, kelas string) (InsertedID interface{}) {
+func InsertMahasiswa(db *mongo.Database, nama string, phone_number string, prodi string, kelas string) (InsertedID interface{}) {
 	var mahasiswa Mahasiswa
 	mahasiswa.Nama = nama
 	mahasiswa.Phone_number = phone_number
 	mahasiswa.Prodi = prodi
 	mahasiswa.Kelas = kelas
-	return InsertOneDoc("dbmhs", "mahasiswa", mahasiswa)
+	return InsertOneDoc(db, "mahasiswa", mahasiswa)
 }
 
-func InsertMatakuliah(nama_matkul string, id_dosen string) (InsertedID interface{}) {
+func InsertMatakuliah(db *mongo.Database, nama_matkul string, id_dosen string) (InsertedID interface{}) {
 	var matakuliah Matakuliah
 	matakuliah.Nama_matkul = nama_matkul
 	matakuliah.ID_dosen = id_dosen
-	return InsertOneDoc("dbmhs", "matakuliah", matakuliah)
+	return InsertOneDoc(db, "matakuliah", matakuliah)
 }
 
-func InsertDosen(nama_dosen string, npm string) (InsertedID interface{}) {
+func InsertDosen(db *mongo.Database, nama_dosen string, npm string) (InsertedID interface{}) {
 	var dosen Dosen
 	dosen.Nama_dosen = nama_dosen
 	dosen.Npm = npm
-	return InsertOneDoc("dbmhs", "dosen", dosen)
+	return InsertOneDoc(db, "dosen", dosen)
 }
 
-func InsertJam_matkul(id_matkul string, jam_masuk string, jam_keluar string) (InsertedID interface{}) {
+func InsertJam_matkul(db *mongo.Database, id_matkul string, jam_masuk string, jam_keluar string) (InsertedID interface{}) {
 	var jam_matkul Jam_matkul
 	jam_matkul.ID_matkul = id_matkul
 	jam_matkul.Jam_masuk = jam_masuk
 	jam_matkul.Jam_keluar = jam_keluar
-	return InsertOneDoc("dbmhs", "Jam_matkul", jam_matkul)
+	return InsertOneDoc(db, "Jam_matkul", jam_matkul)
 }
 
-func GetMahasiswaFromPhone(phone_number string) (staf Mahasiswa) {
-	mahasiswa := MongoConnect("dbmhs").Collection("mahasiswa")
-	// filter := bson.M{"phone_number": phone_number}
+func GetMahasiswaFromPhone(db *mongo.Database, phone_number string) (mhs Mahasiswa) {
+	mahasiswa := db.Collection("mahasiswa")
 	filter := bson.M{"phone_number": phone_number}
-	err := mahasiswa.FindOne(context.TODO(), filter).Decode(&staf)
-	// err := mahasiswa.FindOne(context.TODO(), filter).Decode(&staf)
+	err := mahasiswa.FindOne(context.TODO(), filter).Decode(&mhs)
 	if err != nil {
-		fmt.Printf("getKaryawanFromPhoneNumber: %v\n", err)
+		fmt.Printf("GetKelasFromKodeKelas: %v\n", err)
 	}
-	return staf
+	return mhs
 }
